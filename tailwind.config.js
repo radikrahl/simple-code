3e3117cc660c9b918a6e54a30b7c0933432f9a0d
Andreas Krahl <a.krahl90@gmail.com>
2021-08-16 15:43:56 +0200

    added needed tailwind configuration; added aspect ratio mixin; fixed some additional layout issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,25 @@
 module.exports = {
-    mode: "jit", // Just-In-Time Compiler
-    // purge: ["./src/**/*.html"],
-    darkMode: false, // or 'media' or 'class'
+    /*
+     * disabled right now, because it needs a main.css available
+     * need to fix it in future
+     */
+    // mode: "jit", // Just-In-Time Compiler
+    // purge: ["./**/*.hbs"],
     theme: {
         extend: {},
+        screens: {
+            'xsm': '30em',
+            'sm': '45em',
+            'md': '75em',
+            'lg': '90em',
+            'xl': '120em'
+        }
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [
-        
-    ],
+    corePlugins: [
+        'gridColumn',
+        'gridTemplateColumns',
+        'gap',
+        'padding',
+        'margin'
+    ]
 };
