743a8040cad5439f6f0f1db030be7f213df1bb57
Andreas Krahl <a.krahl90@gmail.com>
2021-09-01 23:08:29 +0200

    mischstand

--- gulpfile.js
+++ gulpfile.js
@@ -87,18 +87,17 @@ function js(done) {
 }
 
 function vendor() {
-    fs.realpath('./node_modules', (x) => console.log(x));
-    const highlightjsStyles = src('./node_modules/@highlightjs/cdn-assets/styles/**')
+    const highlightjsStyles = src('node_modules/@highlightjs/cdn-assets/styles/**')
         .pipe(dest(ensureCreated('./assets/built/highlightjs/styles')));
 
     const highlightjs = src('./node_modules/@highlightjs/cdn-assets/highlight.min.js')
-        .pipe(dest(ensureCreated('./assets/built/highlightjs')));
+        .pipe(dest(ensureCreated('assets/built/highlightjs')));
 
     const fontawesome = src('./node_modules/@fortawesome/fontawesome-free/webfonts/**')
-        .pipe(dest(ensureCreated('./assets/built/fontawesome/webfonts')));
+        .pipe(dest('./assets/built/fontawesome/webfonts'));
 
     const fontawesomeStyles = src('./node_modules/@fortawesome/fontawesome-free/scss/**')
-        .pipe(dest(ensureCreated('./assets/built/fontawesome/scss')));
+        .pipe(dest('assets/built/fontawesome/scss'));
 
     return merge(highlightjs, highlightjsStyles, fontawesome, fontawesomeStyles);
 }
